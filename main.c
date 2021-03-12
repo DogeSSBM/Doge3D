6e33bbb9a6c46aa50494761a07144d4932fa62ef
DogeSSBM <dogessbm@gmail.com>
2021-03-11 21:17:53 -0500

    Automatic commit.

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "Includes.h"
 
-#define MAX_COLUMNS 20
-
 int main(void)
 {
 	// Initialization
@@ -9,29 +7,19 @@ int main(void)
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
 
-	// Define the camera to look into our 3d world (position, target, up vector)
-	Camera camera = { 0 };
-	camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
-	camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
-	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-	camera.fovy = 60.0f;
-	camera.type = CAMERA_PERSPECTIVE;
+	// Define the camera to look into our 3d world
+	Camera3D camera = { 0 };
+	camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };	// Camera looking at point
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };	// Camera up vector (rotation towards target)
+	camera.fovy = 45.0f;	// Camera field-of-view Y
+	camera.type = CAMERA_PERSPECTIVE;	 // Camera mode type
 
-	// Generates some random columns
-	float heights[MAX_COLUMNS] = { 0 };
-	Vector3 positions[MAX_COLUMNS] = { 0 };
-	Color colors[MAX_COLUMNS] = { 0 };
+	Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
 
-	for (int i = 0; i < MAX_COLUMNS; i++)
-	{
-		heights[i] = (float)GetRandomValue(1, 12);
-		positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-		colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
-	}
-
-	SetCameraMode(camera, CAMERA_FIRST_PERSON); // Set a first person camera mode
+	SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
 
 	SetTargetFPS(60);	 // Set our game to run at 60 frames-per-second
 	//--------------------------------------------------------------------------------------
@@ -42,6 +30,8 @@ int main(void)
 		// Update
 		//----------------------------------------------------------------------------------
 		UpdateCamera(&camera);	// Update camera
+
+		if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
 		//----------------------------------------------------------------------------------
 
 		// Draw
@@ -52,26 +42,22 @@ int main(void)
 
 		BeginMode3D(camera);
 
-		DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-		DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);	 // Draw a blue wall
-		DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);	// Draw a green wall
-		DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);	// Draw a yellow wall
+		DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+		DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
 
-		// Draw some cubes around
-		for (int i = 0; i < MAX_COLUMNS; i++)
-		{
-			DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-			DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-		}
+		DrawGrid(10, 1.0f);
 
 		EndMode3D();
 
-		DrawRectangle( 10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
-		DrawRectangleLines( 10, 10, 220, 70, BLUE);
+		DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
+		DrawRectangleLines( 10, 10, 320, 133, BLUE);
 
-		DrawText("First person camera default controls:", 20, 20, 10, BLACK);
-		DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
-		DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
+		DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+		DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+		DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+		DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY);
+		DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
+		DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
